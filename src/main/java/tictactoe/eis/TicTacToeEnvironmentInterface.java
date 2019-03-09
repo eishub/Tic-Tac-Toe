42b6bf59078cd1aa9d7a65ec3c569b478dfeac31
Vincent Koeman <v.j.koeman@tudelft.nl>
2019-03-09 15:37:48 +0100

    update to EIS 0.6.2, update build tools

--- src/main/java/tictactoe/eis/TicTacToeEnvironmentInterface.java
+++ src/main/java/tictactoe/eis/TicTacToeEnvironmentInterface.java
@@ -1,16 +1,10 @@
 package tictactoe.eis;
 
-import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-import tictactoe.game.Configuration;
-import tictactoe.game.TicTacToe;
-import tictactoe.player.PlayerAI;
-import tictactoe.player.PlayerAgent;
-import tictactoe.player.PlayerHuman;
-import tictactoe.player.PlayerKind;
-import tictactoe.player.PlayerType;
 import eis.EIDefaultImpl;
 import eis.exceptions.ActException;
 import eis.exceptions.AgentException;
@@ -26,17 +20,20 @@ import eis.iilang.Identifier;
 import eis.iilang.Numeral;
 import eis.iilang.Parameter;
 import eis.iilang.Percept;
+import tictactoe.game.Configuration;
+import tictactoe.game.TicTacToe;
+import tictactoe.player.PlayerAI;
+import tictactoe.player.PlayerAgent;
+import tictactoe.player.PlayerHuman;
+import tictactoe.player.PlayerKind;
+import tictactoe.player.PlayerType;
 
 /**
  * Environment interface for connecting GOAL agents to Tic Tac Toe game.
- * 
+ *
  * @author K.Hindriks
  */
 public class TicTacToeEnvironmentInterface extends EIDefaultImpl {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	/**
@@ -47,28 +44,23 @@ public class TicTacToeEnvironmentInterface extends EIDefaultImpl {
 
 		/**
 		 * Returns the ENUM constant that corresponds with the key.
-		 * 
-		 * @param key
-		 *            A string indicating a parameter that can be set.
+		 *
+		 * @param key A string indicating a parameter that can be set.
 		 * @return The ENUM constant corresponding to the provided key.
-		 * @throws ManagementException
-		 *             In case key is not recognized as a valid player.
+		 * @throws ManagementException In case key is not recognized as a valid player.
 		 */
 		static InitKey toKey(String key) throws ManagementException {
 			try {
 				return valueOf(key.toUpperCase());
 			} catch (IllegalArgumentException e) {
-				throw new ManagementException(
-						key
-								+ "  is not a valid key for the Tic-Tac-Toe environment",
-						e);
+				throw new ManagementException(key + "  is not a valid key for the Tic-Tac-Toe environment", e);
 			}
 		}
 	}
 
 	/**
 	 * Types of players.
-	 * 
+	 *
 	 * @author W.Pasman 5jul12
 	 */
 	private enum PlayerReasoner {
@@ -87,43 +79,37 @@ public class TicTacToeEnvironmentInterface extends EIDefaultImpl {
 
 		/**
 		 * accepts the values as string and returns the proper enum.
-		 * 
-		 * @param engine
-		 *            is string, case insensitive.
+		 *
+		 * @param engine is string, case insensitive.
 		 * @return PlayerEngine type.
-		 * @throws ManagementException
-		 *             In case engine was not recognized as a valid
-		 *             {@link PlayerReasoner}.
+		 * @throws ManagementException In case engine was not recognized as a valid
+		 *                             {@link PlayerReasoner}.
 		 */
-		public static PlayerReasoner fromString(String engine)
-				throws ManagementException {
+		public static PlayerReasoner fromString(String engine) throws ManagementException {
 			try {
 				return PlayerReasoner.valueOf(engine.toUpperCase());
 			} catch (IllegalArgumentException e) {
-				throw new ManagementException("Did not recognize " + engine
-						+ " as a valid player.", e);
+				throw new ManagementException("Did not recognize " + engine + " as a valid player.", e);
 			}
 		}
 	};
 
 	/**
-	 * Only after informing both players about the end of game state the state
-	 * of the environment can be transitioned to {link
-	 * eis.EnvironmentState.PAUSED} TODO Link not working.
+	 * Only after informing both players about the end of game state the state of
+	 * the environment can be transitioned to {link eis.EnvironmentState.PAUSED}
+	 * TODO Link not working.
 	 */
 	private boolean informedXPlayerOfEndOfGame = false;
 	private boolean informedOPlayerOfEndOfGame = false;
 
 	/**
 	 * Launches the game.
-	 * 
-	 * @param argv
-	 *            Arguments are ignored.
+	 *
+	 * @param argv Arguments are ignored.
 	 */
 	public static void main(String argv[]) {
 		// Set default initial players: X player is a human, O player is AI.
-		TicTacToe.getTicTacToe()
-				.setXPlayer(new PlayerHuman(PlayerType.XPLAYER));
+		TicTacToe.getTicTacToe().setXPlayer(new PlayerHuman(PlayerType.XPLAYER));
 		TicTacToe.getTicTacToe().setOPlayer(new PlayerAI(PlayerType.OPLAYER));
 		// Launch GUI.
 		TicTacToe.getGui();
@@ -132,10 +118,10 @@ public class TicTacToeEnvironmentInterface extends EIDefaultImpl {
 	}
 
 	/**
-	 * Launches the Tic-Tac-Toe game upon loading the EIS interface. Just a stub
-	 * to get started. We need initialization information before we can start
-	 * setting up things. E.g., entities can be made available only when we know
-	 * what type of entities we should be creating.
+	 * Launches the Tic-Tac-Toe game upon loading the EIS interface. Just a stub to
+	 * get started. We need initialization information before we can start setting
+	 * up things. E.g., entities can be made available only when we know what type
+	 * of entities we should be creating.
 	 */
 	public TicTacToeEnvironmentInterface() {
 	}
@@ -145,33 +131,22 @@ public class TicTacToeEnvironmentInterface extends EIDefaultImpl {
 	/**************************************************************/
 	/**
 	 *
-	 * @param position
-	 *            position
-	 * @param player
-	 *            player
+	 * @param position position
+	 * @param player   player
 	 */
 	public void actionoccupy(int position, String player) {
-
 		PlayerType playerType = PlayerType.fromString(player);
 		Configuration currentConfiguration = TicTacToe.getConfiguration();
 
-		if (currentConfiguration.getCurrentPlayerType() == playerType) { // also
-																			// checks
-																			// whether
-																			// game
-																			// has
-																			// not
-																			// yet
-																			// ended
+		if (currentConfiguration.getCurrentPlayerType() == playerType) {
+			// also checks whether game has not yet ended
 			if (TicTacToe.getConfiguration().getPlayerAtSquare(position - 1) == PlayerType.NONE) {
-				TicTacToe.getConfiguration().setPlayerAtSquare(position - 1,
-						playerType);
+				TicTacToe.getConfiguration().setPlayerAtSquare(position - 1, playerType);
 				if (TicTacToe.showGui) {
 					TicTacToe.getGui().getRootPane().repaint();
 				}
 
-				Percept percept = new Percept("pos", new Numeral(position),
-						new Identifier(playerType.toString()));
+				Percept percept = new Percept("pos", new Numeral(position), new Identifier(playerType.toString()));
 				try {
 					notifyAgents(percept);
 				} catch (EnvironmentInterfaceException e) {
@@ -184,10 +159,8 @@ public class TicTacToeEnvironmentInterface extends EIDefaultImpl {
 			// Check if computer (ai) player needs to move
 			if (!currentConfiguration.isEndOfGame()) {
 				if (currentConfiguration.getCurrentPlayer().getKindofPlayer() == PlayerKind.AI) {
-					int p = ((PlayerAI) currentConfiguration.getCurrentPlayer())
-							.coupOrdi();
-					currentConfiguration.setPlayerAtSquare(p,
-							currentConfiguration.getCurrentPlayerType());
+					int p = ((PlayerAI) currentConfiguration.getCurrentPlayer()).coupOrdi();
+					currentConfiguration.setPlayerAtSquare(p, currentConfiguration.getCurrentPlayerType());
 					if (TicTacToe.getGui().getGameBoard().isSoundEnabled()) {
 						TicTacToe.getGui().getGameBoard().getGameSound().play();
 					}
@@ -204,24 +177,19 @@ public class TicTacToeEnvironmentInterface extends EIDefaultImpl {
 	/**************************************************************/
 
 	/**
-	 * Returns current board configuration as list of percepts and the player
-	 * whose turn it is.
+	 * Returns current board configuration as list of percepts and the player whose
+	 * turn it is.
 	 */
 	@Override
-	protected LinkedList<Percept> getAllPerceptsFromEntity(String entity)
-			throws PerceiveException, NoEnvironmentException {
+	protected List<Percept> getAllPerceptsFromEntity(String entity) throws PerceiveException, NoEnvironmentException {
+		List<Percept> percepts = new LinkedList<>();
 
-		LinkedList<Percept> percepts = new LinkedList<Percept>();
-
-		PlayerType currentPlayer = TicTacToe.getConfiguration()
-				.getCurrentPlayerType();
+		PlayerType currentPlayer = TicTacToe.getConfiguration().getCurrentPlayerType();
 		if (currentPlayer != PlayerType.NONE) {
-			percepts.add(new Percept("turn", new Identifier(currentPlayer
-					.toString())));
+			percepts.add(new Percept("turn", new Identifier(currentPlayer.toString())));
 		}
 
-		PlayerType[][] gameConfiguration = TicTacToe.getConfiguration()
-				.getGameBoardConfiguration();
+		PlayerType[][] gameConfiguration = TicTacToe.getConfiguration().getGameBoardConfiguration();
 
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
@@ -233,8 +201,7 @@ public class TicTacToeEnvironmentInterface extends EIDefaultImpl {
 		PlayerType winner = TicTacToe.getConfiguration().getWinner();
 		if (winner != null) {
 			if (winner != PlayerType.NONE) {
-				percepts.add(new Percept("winner", new Identifier(winner
-						.toString())));
+				percepts.add(new Percept("winner", new Identifier(winner.toString())));
 			} else {
 				percepts.add(new Percept("draw"));
 			}
@@ -243,14 +210,12 @@ public class TicTacToeEnvironmentInterface extends EIDefaultImpl {
 				this.informedOPlayerOfEndOfGame |= (InitKey.toKey(entity) == InitKey.OPLAYER);
 				// Game has ended. If both players have been informed, set
 				// environment state to PAUSED.
-				if (this.informedXPlayerOfEndOfGame
-						&& this.informedOPlayerOfEndOfGame) {
+				if (this.informedXPlayerOfEndOfGame && this.informedOPlayerOfEndOfGame) {
 					setState(EnvironmentState.PAUSED);
 				}
 			} catch (ManagementException e) {
 				// TODO: for now the best we can do??
-				System.out
-						.print("End of Tic-Tac-Toe game but could not change EIS to PAUSED state.");
+				System.out.print("End of Tic-Tac-Toe game but could not change EIS to PAUSED state.");
 			}
 		}
 
@@ -259,7 +224,7 @@ public class TicTacToeEnvironmentInterface extends EIDefaultImpl {
 
 	/**
 	 * All entities can perform the action 'occupy' and only that action.
-	 * 
+	 *
 	 * @return true if action name is 'occupy'; false otherwise.
 	 */
 	@Override
@@ -281,49 +246,36 @@ public class TicTacToeEnvironmentInterface extends EIDefaultImpl {
 	}
 
 	@Override
-	protected Percept performEntityAction(String entity, Action action)
-			throws ActException {
-
-		int position;
-
+	protected Percept performEntityAction(String entity, Action action) throws ActException {
 		if (!isSupportedByEntity(action, entity)) {
-			throw new ActException(
-					ActException.FAILURE,
-					"action "
-							+ action
-							+ "is not supported; only 'occupy' action with single integer argument is supported.");
+			throw new ActException(ActException.FAILURE, "action " + action
+					+ "is not supported; only 'occupy' action with single integer argument is supported.");
 		}
 
 		/* first check parameter */
-		LinkedList<Parameter> params = action.getParameters();
+		List<Parameter> params = action.getParameters();
 		if (params.size() != 1) {
-			throw new ActException(ActException.FAILURE, "action " + action
-					+ "has single argument, but found argument(s) " + params);
+			throw new ActException(ActException.FAILURE,
+					"action " + action + "has single argument, but found argument(s) " + params);
 		}
-		if (!(params.getFirst() instanceof Numeral)) {
-			throw new ActException(ActException.FAILURE, "action " + action
-					+ "should have integer argument, but found argument "
-					+ params);
+		if (!(params.get(0) instanceof Numeral)) {
+			throw new ActException(ActException.FAILURE,
+					"action " + action + "should have integer argument, but found argument " + params);
 		}
-		position = ((Numeral) params.getFirst()).getValue().intValue();
+		int position = ((Numeral) params.get(0)).getValue().intValue();
 		if (position < 1 || position > 9) {
-			throw new ActException(
-					ActException.FAILURE,
-					"action "
-							+ action
-							+ "should have integer argument between 1 and 9, but found "
-							+ params);
+			throw new ActException(ActException.FAILURE,
+					"action " + action + "should have integer argument between 1 and 9, but found " + params);
 		}
 
 		actionoccupy(position, entity);
 
-		return null; // TODO: new Percept("pos", new Numeral(position), new
-						// Identifier());
+		return null;
+		// TODO: new Percept("pos", new Numeral(position), new Identifier());
 	}
 
 	@Override
-	public void init(Map<String, Parameter> parameters)
-			throws ManagementException {
+	public void init(Map<String, Parameter> parameters) throws ManagementException {
 		setState(EnvironmentState.INITIALIZING);
 		TicTacToe.getTicTacToe();
 		processParameters(parameters);
@@ -363,8 +315,7 @@ public class TicTacToeEnvironmentInterface extends EIDefaultImpl {
 	}
 
 	@Override
-	public void reset(Map<String, Parameter> parameters)
-			throws ManagementException {
+	public void reset(Map<String, Parameter> parameters) throws ManagementException {
 		processParameters(parameters);
 		this.informedXPlayerOfEndOfGame = false;
 		this.informedOPlayerOfEndOfGame = false;
@@ -383,32 +334,29 @@ public class TicTacToeEnvironmentInterface extends EIDefaultImpl {
 
 	/**
 	 * Get current reward.
-	 * 
-	 * @return "1" if the current player has won the game; "-1" if other player
-	 *         has won the game; "0" otherwise.
+	 *
+	 * @return "1" if the current player has won the game; "-1" if other player has
+	 *         won the game; "0" otherwise.
 	 */
 	@Override
 	public String queryProperty(String property) throws QueryException {
 		if (property.startsWith("REWARD ")) {
-			HashSet<String> entities;
+			Set<String> entities;
 			String agentName = property.substring(REWARD.length());
 			try {
 				entities = getAssociatedEntities(agentName);
 			} catch (AgentException e) {
-				throw new QueryException("can't get entities associated with "
-						+ agentName, e);
+				throw new QueryException("can't get entities associated with " + agentName, e);
 			}
 			if (entities.size() != 1) {
-				throw new QueryException("agent " + agentName
-						+ " has not exactly 1 associated entity:" + entities);
+				throw new QueryException("agent " + agentName + " has not exactly 1 associated entity:" + entities);
 			}
 			String entity = entities.iterator().next();
 			PlayerType playerType = PlayerType.fromString(entity);
 			PlayerType winner = TicTacToe.getConfiguration().getWinner();
-			if (winner == null || winner == playerType.NONE) {
+			if (winner == null || winner == PlayerType.NONE) {
 				return "0"; // game still running or ended in draw
-			}
-			if (winner == playerType) {
+			} else if (winner == playerType) {
 				return "1";
 			} else {
 				return "-1";
@@ -419,13 +367,11 @@ public class TicTacToeEnvironmentInterface extends EIDefaultImpl {
 
 	/**
 	 * DOC
-	 * 
+	 *
 	 * @param parameters
 	 * @throws ManagementException
 	 */
-	private void processParameters(Map<String, Parameter> parameters)
-			throws ManagementException {
-
+	private void processParameters(Map<String, Parameter> parameters) throws ManagementException {
 		// GUI is enabled by default
 		// boolean guimode = true;
 
@@ -437,68 +383,54 @@ public class TicTacToeEnvironmentInterface extends EIDefaultImpl {
 			switch (InitKey.toKey(key)) {
 			case XPLAYER: // that is player 1
 				if (value instanceof Identifier) {
-					switch (PlayerReasoner.fromString(((Identifier) value)
-							.getValue())) {
+					switch (PlayerReasoner.fromString(((Identifier) value).getValue())) {
 					case HUMAN:
-						TicTacToe.getTicTacToe().setPlayer1(
-								new PlayerHuman(PlayerType.XPLAYER));
+						TicTacToe.getTicTacToe().setPlayer1(new PlayerHuman(PlayerType.XPLAYER));
 						break;
 					case AGENT:
-						TicTacToe.getTicTacToe().setPlayer1(
-								new PlayerAgent(PlayerType.XPLAYER));
+						TicTacToe.getTicTacToe().setPlayer1(new PlayerAgent(PlayerType.XPLAYER));
 						break;
 					case AI:
-						TicTacToe.getTicTacToe().setPlayer1(
-								new PlayerAI(PlayerType.XPLAYER));
+						TicTacToe.getTicTacToe().setPlayer1(new PlayerAI(PlayerType.XPLAYER));
 						break;
 					default:
-						throw new ManagementException(
-								"Player type 'human', 'agent', or 'ai' expected as value for key"
-										+ "'xplayer' but got " + value);
+						throw new ManagementException("Player type 'human', 'agent', or 'ai' expected as value for key"
+								+ "'xplayer' but got " + value);
 					}
 				} else {
-					throw new ManagementException(
-							"Player type 'human', 'agent', or 'ai' expected as value for key"
-									+ "'xplayer' but got " + value);
+					throw new ManagementException("Player type 'human', 'agent', or 'ai' expected as value for key"
+							+ "'xplayer' but got " + value);
 				}
 
 				// make entity available
 				break;
 			case OPLAYER: // that is player 2
 				if (value instanceof Identifier) {
-					switch (PlayerReasoner.fromString(((Identifier) value)
-							.getValue())) {
+					switch (PlayerReasoner.fromString(((Identifier) value).getValue())) {
 					case HUMAN:
-						TicTacToe.getTicTacToe().setPlayer2(
-								new PlayerHuman(PlayerType.OPLAYER));
+						TicTacToe.getTicTacToe().setPlayer2(new PlayerHuman(PlayerType.OPLAYER));
 						break;
 					case AGENT:
-						TicTacToe.getTicTacToe().setPlayer2(
-								new PlayerAgent(PlayerType.OPLAYER));
+						TicTacToe.getTicTacToe().setPlayer2(new PlayerAgent(PlayerType.OPLAYER));
 						break;
 					case AI:
-						TicTacToe.getTicTacToe().setPlayer2(
-								new PlayerAI(PlayerType.OPLAYER));
+						TicTacToe.getTicTacToe().setPlayer2(new PlayerAI(PlayerType.OPLAYER));
 						break;
 					default:
-						throw new ManagementException(
-								"Player type 'human', 'agent', or 'ai' expected as value for key"
-										+ "'xplayer' but got " + value);
+						throw new ManagementException("Player type 'human', 'agent', or 'ai' expected as value for key"
+								+ "'xplayer' but got " + value);
 					}
 				} else {
-					throw new ManagementException(
-							"Player type 'human', 'agent', or 'ai' expected as value for key"
-									+ "'xplayer' but got " + value);
+					throw new ManagementException("Player type 'human', 'agent', or 'ai' expected as value for key"
+							+ "'xplayer' but got " + value);
 				}
 				break;
 			case GUI:
 				if (value instanceof Identifier) {
-					TicTacToe.showGui = Boolean
-							.parseBoolean(((Identifier) value).getValue());
+					TicTacToe.showGui = Boolean.parseBoolean(((Identifier) value).getValue());
 				} else {
 					throw new ManagementException(
-							"\"true\" or \"false\" expected as value for key "
-									+ "\"gui\" but got " + value);
+							"\"true\" or \"false\" expected as value for key " + "\"gui\" but got " + value);
 				}
 				break;
 			}
@@ -507,12 +439,11 @@ public class TicTacToeEnvironmentInterface extends EIDefaultImpl {
 
 	/**
 	 * Provides the name of the environment.
-	 * 
+	 *
 	 * @return The name of the environment.
 	 */
 	@Override
 	public String toString() {
 		return "Tic-Tac-Toe";
 	}
-
 }
